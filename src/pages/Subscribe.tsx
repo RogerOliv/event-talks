ed02d75db0aaa204049640c870daeb6e2bc84673
RogerOliv <rogerpinhooliveira@live.com>
2022-06-25 20:04:01 -0300

    Missing Image

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -2,6 +2,7 @@ import { gql, useMutation } from "@apollo/client";
 import { useState, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
+import imgUrl from "../assets/code-mockup.png";
 import { useCreateSubscriberMutation } from "../graphql/generated";
 
 export function Subscribe() {
@@ -61,7 +62,7 @@ export function Subscribe() {
         </div>
       </div>
 
-      <img src="/src/assets/code-mockup.png" className="mt-10" alt="" />
+      <img src={imgUrl} className="mt-10" alt="" />
     </div>
   )
 }
